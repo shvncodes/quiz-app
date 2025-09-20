8bd206105512955914e203711992a4e861f29106
Shivani Choudhary <shivanichoudhary2206@gmail.com>
2025-09-20 23:24:06 +0530

    export all quiz questions and import in script.js file

---
+++ questions.js
@@ -0,0 +1,122 @@
+export const questions = [
+    {
+        id: 'q1',
+        name: 'Who is known as the Father of the Indian Constitution?',
+        options: ['Mahatma Gandhi', 'B. R. Ambedkar', 'Jawaharlal Nehru', 'Rajendra Prasad'],
+        rightOption: 'B. R. Ambedkar'
+    },
+    {
+        id: 'q2',
+        name: 'Which is the national animal of India?',
+        options: ['Elephant', 'Bengal Tiger', 'Lion', 'Leopard'],
+        rightOption: 'Bengal Tiger'
+    },
+    {
+        id: 'q3',
+        name: 'Who was the first Prime Minister of India?',
+        options: ['Sardar Patel', 'Mahatma Gandhi', 'Jawaharlal Nehru', 'Subhas Chandra Bose'],
+        rightOption: 'Jawaharlal Nehru'
+    },
+    {
+        id: 'q4',
+        name: 'Which is the largest state of India by area?',
+        options: ['Madhya Pradesh', 'Maharashtra', 'Rajasthan', 'Uttar Pradesh'],
+        rightOption: 'Rajasthan'
+    },
+    {
+        id: 'q5',
+        name: 'Which Indian city is known as the "Pink City"?',
+        options: ['Jodhpur', 'Jaipur', 'Udaipur', 'Jaisalmer'],
+        rightOption: 'Jaipur'
+    },
+    {
+        id: 'q6',
+        name: 'In which year did India gain independence?',
+        options: ['1945', '1947', '1950', '1952'],
+        rightOption: '1947'
+    },
+    {
+        id: 'q7',
+        name: 'Which is the national flower of India?',
+        options: ['Rose', 'Sunflower', 'Lotus', 'Lily'],
+        rightOption: 'Lotus'
+    },
+    {
+        id: 'q8',
+        name: 'Which Indian scientist is known as the "Missile Man of India"?',
+        options: ['Homi J. Bhabha', 'C. V. Raman', 'A. P. J. Abdul Kalam', 'Vikram Sarabhai'],
+        rightOption: 'A. P. J. Abdul Kalam'
+    },
+    {
+        id: 'q9',
+        name: 'Which is the longest river in India?',
+        options: ['Yamuna', 'Brahmaputra', 'Ganga', 'Godavari'],
+        rightOption: 'Ganga'
+    },
+    {
+        id: 'q10',
+        name: 'Who was the first woman Prime Minister of India?',
+        options: ['Sarojini Naidu', 'Indira Gandhi', 'Pratibha Patil', 'Sushma Swaraj'],
+        rightOption: 'Indira Gandhi'
+    },
+    {
+        id: 'q11',
+        name: 'Which festival is also known as the "Festival of Lights"?',
+        options: ['Holi', 'Diwali', 'Dussehra', 'Pongal'],
+        rightOption: 'Diwali'
+    },
+    {
+        id: 'q12',
+        name: 'What is the national currency of India?',
+        options: ['Rupee', 'Taka', 'Dollar', 'Yen'],
+        rightOption: 'Rupee'
+    },
+    {
+        id: 'q13',
+        name: 'Which Indian state is famous for tea plantations?',
+        options: ['Kerala', 'Assam', 'Punjab', 'Rajasthan'],
+        rightOption: 'Assam'
+    },
+    {
+        id: 'q14',
+        name: 'Where is the Indian Space Research Organisation (ISRO) headquarters located?',
+        options: ['Mumbai', 'New Delhi', 'Bengaluru', 'Hyderabad'],
+        rightOption: 'Bengaluru'
+    },
+    {
+        id: 'q15',
+        name: 'Which monument is called the "Symbol of Love"?',
+        options: ['Red Fort', 'India Gate', 'Taj Mahal', 'Qutub Minar'],
+        rightOption: 'Taj Mahal'
+    },
+    {
+        id: 'q16',
+        name: 'Who wrote the Indian national anthem "Jana Gana Mana"?',
+        options: ['Bankim Chandra Chatterjee', 'Rabindranath Tagore', 'Subhas Chandra Bose', 'Mahatma Gandhi'],
+        rightOption: 'Rabindranath Tagore'
+    },
+    {
+        id: 'q17',
+        name: 'Which state is known as the "Land of Rising Sun" in India?',
+        options: ['Arunachal Pradesh', 'Sikkim', 'Nagaland', 'Manipur'],
+        rightOption: 'Arunachal Pradesh'
+    },
+    {
+        id: 'q18',
+        name: 'Which Indian sportsperson is also called the "God of Cricket"?',
+        options: ['Virat Kohli', 'Kapil Dev', 'Sachin Tendulkar', 'Sunil Gavaskar'],
+        rightOption: 'Sachin Tendulkar'
+    },
+    {
+        id: 'q19',
+        name: 'Which Indian classical dance form is famous in Tamil Nadu?',
+        options: ['Kathak', 'Odissi', 'Bharatanatyam', 'Kuchipudi'],
+        rightOption: 'Bharatanatyam'
+    },
+    {
+        id: 'q20',
+        name: 'Which Indian state has "Kolkata" as its capital?',
+        options: ['Odisha', 'Assam', 'West Bengal', 'Jharkhand'],
+        rightOption: 'West Bengal'
+    }
+]
