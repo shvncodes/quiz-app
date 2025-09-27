573b85088c101ed073e73892e7f931fde5a7e5f3
Shivani Choudhary <shivanichoudhary2206@gmail.com>
2025-09-27 17:00:19 +0530

    quiz app

--- script.js
+++ script.js
@@ -1,28 +1,115 @@
-/* -> Create 3 page : start, quiz, score
-1. startPage:- basic layout when user click on Start Quiz button start and last page hide and show mid page.
-2. quizPage:- basic layout
-            - timer start (02:00)
-            - Upadte Score
-            - End Quiz button -> startPage & midPage hide and show last page with score
-            - Skip button: doesn't count it score and move to next question
-            - Next button: add it score on currentScore and move to next question
-            - if(it last question(10)) then after click on Submit button: add it score and show last page
-3. scorePage:- basic layout
-            - show total score
-            - if(score >= 20) then show pass message else show fail message 
-*/
-import { questions } from "./questions.js"; 
+import { questions } from "./questions.js";
 const startPage = document.querySelector("#startPage");
 const userName = document.querySelector("#nameInput");
 const quizStartBtn = document.querySelector("#quizStartBtn");
 const quizPage = document.querySelector("#quizPage");
+const ques = document.querySelector("#question");
+const optionsNode = document.querySelector("#options");
+const skipBtn = document.querySelector("#skipBtn");
+const nextQuesBtn = document.querySelector("#nextQuesBtn");
+const quizEndBtn = document.querySelector("#quizEndBtn");
 const scorePage = document.querySelector("#scorePage");
+const result = document.querySelector("#totalScore");
+const resultMsgNode = document.querySelector("#resultMsg");
+
+let currentScore = 0;
+let currentQuestionIndx = 0;
+
+function showResult() {
+    quizPage.style.display = 'none';
+    scorePage.style.display = 'flex';
+
+    const totalScore = 4*(questions.length);
+    result.textContent = `Your total score is: ${currentScore} out of ${totalScore}`;
+    if(currentScore >= 20) {
+       resultMsgNode.textContent = "Congratulations you passed this Quiz!!";
+       resultMsgNode.style.color = 'green';
+    } else {
+        resultMsgNode.textContent = "You failed this Quiz, Better luck next time!!";
+        resultMsgNode.style.color = 'red';   
+    }
+}
+
+function timer() {
+    const timerNode = document.getElementById("timer");
+    const initialTime = 2 * 60;
+    let totalSeconds = initialTime;
+    let intervalId = null;
+
+    function updatedisplay() {
+        const minutes = String(Math.floor(totalSeconds / 60)).padStart(2, "0");
+        const seconds = String(totalSeconds % 60).padStart(2, "0");
+        timerNode.textContent = `Timer: ${minutes}:${seconds}`;
+    }
+
+    function startTimer() {
+        if (intervalId) return;
+        updatedisplay();
+
+        intervalId = setInterval(() => {
+            totalSeconds--;
+            updatedisplay();
+
+            if (totalSeconds <= 0) {
+                clearInterval(intervalId);
+                intervalId = null;
+                showResult();
+            }
+        }, 1000);
+    }
+
+    startTimer();
+};
 
 quizStartBtn.addEventListener("click", () => {
-    if(!userName.value.trim()) {
+    if (!userName.value.trim()) {
         alert('Please enter your name to start the quiz.');
         return;
     }
     startPage.style.display = 'none';
     quizPage.style.display = 'flex';
+
+    timer();
+    insertQuestionInDOM(questions[currentQuestionIndx]);
 })
+
+function insertQuestionInDOM(question) {
+    ques.textContent = `Q.${currentQuestionIndx+1}: `+question.name;
+    let optionsHTML = "";
+    question.options.forEach((option, i) => {
+       optionsHTML += `<div id="option${i}">
+            <input id="opt${i}" name="option" type="radio" value='${option}'>
+            <label for="opt${i}">${option}</label>
+        </div>\n`
+    })  
+    optionsNode.innerHTML = optionsHTML;
+}
+
+skipBtn.addEventListener("click", function() {
+    if(currentQuestionIndx === questions.length - 1) {
+        showResult();
+        return;
+    }
+    currentQuestionIndx++;
+    insertQuestionInDOM(questions[currentQuestionIndx]);
+})
+
+nextQuesBtn.addEventListener("click", function() {
+    if(currentQuestionIndx === questions.length - 1) {
+        showResult();
+        return;
+    }
+    const selectedInput = document.querySelector('input[name = "option"]:checked');
+    if(!selectedInput) {
+        alert("Select an option");
+        return;
+    } 
+    if(selectedInput.value === questions[currentQuestionIndx].rightOption) currentScore += 4;
+    else currentScore -= 1;
+
+    currentQuestionIndx++;
+    insertQuestionInDOM(questions[currentQuestionIndx]);
+})
+
+
+quizEndBtn.addEventListener("click", showResult);
